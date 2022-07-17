05116c83762a2a1e48e262ef579e754bc42bb8eb
April Reitan <45362322+areitan@users.noreply.github.com>
2022-07-16 21:46:51 -0700

    adding initial Contact.js code

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -4,9 +4,14 @@ export default function Contact() {
   return (
     <div>
       <h1>Contact Page</h1>
-      <p>
-        
-      </p>
+      <div class="contact-me">
+        <h2 id="contact-me">Contact Me</h2>
+        <p><a href="mailto:areitan@fredhutch.org">areitan@fredhutch.org</a></p>
+        <p><a href="https://github.com/areitan">GitHub</a></p>
+        <p><a href="https://www.linkedin.com/in/april-reitan-4217559/">LinkedIn</a></p>
+        <p><a href="tel:2535081957">Click to call</a></p>
+      </div>
     </div>
+    </div >
   );
 }
