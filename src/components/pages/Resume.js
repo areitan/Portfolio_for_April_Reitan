1d45bbc1d5fbd652218b5a2143b01ab0da59cab9
April Reitan <45362322+areitan@users.noreply.github.com>
2022-07-16 23:39:11 -0700

    adding resume link to resume page.

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -5,7 +5,7 @@ export default function Resume() {
     <div>
       <h1>My Resume</h1>
       <p>
-       
+      <a href="April_Reitan_Resume_6_15_2022.pdf" download>Resume <i className="fa">&#xf019;</i></a>
       </p>
     </div>
   );
