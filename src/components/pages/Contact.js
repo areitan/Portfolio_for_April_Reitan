a71a6962fd30e84b800e6d5c863b6ebbe5720c26
April Reitan <45362322+areitan@users.noreply.github.com>
2022-07-16 22:04:48 -0700

    updating html classes to js className all pages.

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -4,7 +4,7 @@ export default function Contact() {
   return (
     <div>
       <h1>Contact Page</h1>
-      <div class="contact-me">
+      <div className="contact-me">
         <h2 id="contact-me">Contact Me</h2>
         <p><a href="mailto:areitan@fredhutch.org">areitan@fredhutch.org</a></p>
         <p><a href="https://github.com/areitan">GitHub</a></p>
@@ -12,6 +12,5 @@ export default function Contact() {
         <p><a href="tel:2535081957">Click to call</a></p>
       </div>
     </div>
-    </div >
   );
 }
--- src/components/pages/Project.js
+++ src/components/pages/Project.js
@@ -2,33 +2,33 @@ import React from 'react';
 
 export default function Project() {
   return (
-    <div class="work">
+    <div className="work">
       <h2 id="work">Work</h2>
-      <div class="pic-grid">
-        <div class="box-item item1">
-          <a href="https://areitan.github.io/PocketMonster/"><img class="big" alt="PocketMonster"
+      <div className="pic-grid">
+        <div className="box-item item1">
+          <a href="https://areitan.github.io/PocketMonster/"><img className="big" alt="PocketMonster"
             src="./assets/images/index_screenshot.png"></a>
-          <div class="text-position">PocketMonster</div>
+          <div className="text-position">PocketMonster</div>
         </div>
-        <div class="box-item item2">
+        <div className="box-item item2">
           <a href="https://areitan.github.io/SemanticMarkup/"><img alt="Semantic Markup"
             src="./assets/images/Accessible_Wesite_Display.png"></a>
-          <div class="text-position">Semantic Markup</div>
+          <div className="text-position">Semantic Markup</div>
         </div>
-        <div class="box-item item3">
+        <div className="box-item item3">
           <a href="https://areitan.github.io/Password_Generator/"><img alt="Password Generator"
             src="./assets/images/password.png"></a>
-          <div class="text-position">Password Generator</div>
+          <div className="text-position">Password Generator</div>
         </div>
-        <div class="box-item item4">
+        <div className="box-item item4">
           <a href="https://areitan.github.io/Day_Planner/"><img alt="Day Planner"
             src="./assets/images/Day_Planner.png"></a>
-          <div class="text-position">Day Planner</div>
+          <div className="text-position">Day Planner</div>
         </div>
-        <div class="box-item item5">
+        <div className="box-item item5">
           <a href="https://drive.google.com/file/d/1g5jCev9P6KtSrnee9WpiLV1jh25YpQS9/view"><img alt="Team Profile Generator"
             src="./assets/images/5_ViewProfile.png"></a>
-          <div class="text-position">Team Profile Generator</div>
+          <div className="text-position">Team Profile Generator</div>
         </div>
       </div>
     </div>
