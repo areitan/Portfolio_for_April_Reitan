eece23b22450899fdec6d0086bfdd9f4bf266c87
April Reitan <45362322+areitan@users.noreply.github.com>
2022-07-16 22:41:10 -0700

    adding initial code for header and footer components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
 import React from "react";
+import Header from './components/Header';
 import PortfolioContainer from "./components/PortfolioContainer";
+import Header from './components/Footer';
 
-const App = () => <PortfolioContainer />;
 
+function App() {
+    return (
+      <div>
+        <Header />
+        <PortfolioContainer />
+        <Footer />
+      </div>
+    );
+  }
 export default App;
