d2891fa6908bf9dfc02ea30c5a61ba5158f5e6f9
April Reitan <45362322+areitan@users.noreply.github.com>
2022-07-16 21:45:02 -0700

    adding beginning Project.js code

--- src/components/pages/Project.js
+++ src/components/pages/Project.js
@@ -2,11 +2,35 @@ import React from 'react';
 
 export default function Project() {
   return (
-    <div>
-      <h1>Project Page</h1>
-      <p>
-       
-      </p>
+    <div class="work">
+      <h2 id="work">Work</h2>
+      <div class="pic-grid">
+        <div class="box-item item1">
+          <a href="https://areitan.github.io/PocketMonster/"><img class="big" alt="PocketMonster"
+            src="./assets/images/index_screenshot.png"></a>
+          <div class="text-position">PocketMonster</div>
+        </div>
+        <div class="box-item item2">
+          <a href="https://areitan.github.io/SemanticMarkup/"><img alt="Semantic Markup"
+            src="./assets/images/Accessible_Wesite_Display.png"></a>
+          <div class="text-position">Semantic Markup</div>
+        </div>
+        <div class="box-item item3">
+          <a href="https://areitan.github.io/Password_Generator/"><img alt="Password Generator"
+            src="./assets/images/password.png"></a>
+          <div class="text-position">Password Generator</div>
+        </div>
+        <div class="box-item item4">
+          <a href="https://areitan.github.io/Day_Planner/"><img alt="Day Planner"
+            src="./assets/images/Day_Planner.png"></a>
+          <div class="text-position">Day Planner</div>
+        </div>
+        <div class="box-item item5">
+          <a href="https://drive.google.com/file/d/1g5jCev9P6KtSrnee9WpiLV1jh25YpQS9/view"><img alt="Team Profile Generator"
+            src="./assets/images/5_ViewProfile.png"></a>
+          <div class="text-position">Team Profile Generator</div>
+        </div>
+      </div>
     </div>
   );
 }
