7ab819a9029a145c9d9897523a1259825c902de1
April Reitan <45362322+areitan@users.noreply.github.com>
2022-07-20 22:47:33 -0700

    adding styling to resume

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,13 +1,18 @@
 import React from 'react';
 
+const styles = {
+  resume: {
+    fontSize: "30px"
+  }
+};
 
 export default function Resume() {
   return (
     <div>
       <h1>My Resume</h1>
-      <p>
-      <a href="April_Reitan_Resume_6_15_2022.pdf" download>Resume <i className="fa">&#xf019;</i></a>
-      </p>
+      <div style={styles.resume} className='card text-center'>
+      <a href="April_Reitan_Resume_6_15_2022.pdf" download>Download My Resume</a>
+      </div>
     </div>
   );
 }
