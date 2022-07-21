24cc403964bcd8da7558a8657c96a8481e5b8c22
April Reitan <45362322+areitan@users.noreply.github.com>
2022-07-20 22:29:59 -0700

    adding styling for footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,21 +4,24 @@ const styles = {
   contactMe: {
     display: "flex",
     flexDirection: "row",
-    justifyContent: "spaceBetween",
-    alignItems: "spaceBetween",
-    fontSize: "25px",
-    justifyContent: "spaceEvenly",
+    justifyText: "center",
+    fontSize: "20px"
   }
 };
 
 function Footer() {
   return (
-    <div>
-      <div style={styles.contactMe}>
-        <h2 id="contact-me">Contact Me</h2>
-        <p><a href="mailto:areitan@fredhutch.org">areitan@fredhutch.org</a></p><br />
-        <p><a href="https://github.com/areitan">GitHub</a></p><br />
-        <p><a href="https://www.linkedin.com/in/april-reitan-4217559/">LinkedIn</a></p><br />
+    <div style={styles.contactMe} className='row '>
+      <div className='card col-3 text-center'>
+        <p><a href="mailto:areitan@fredhutch.org">areitan@fredhutch.org</a></p>
+      </div>
+      <div className='card col-3 text-center'>
+        <p><a href="https://github.com/areitan">GitHub</a></p>
+      </div>
+      <div className='card col-3 text-center'>
+        <p><a href="https://www.linkedin.com/in/april-reitan-4217559/">LinkedIn</a></p>
+      </div>
+      <div className='card col-3 text-center'>
         <p><a href="tel:2535081957">Click to call</a></p>
       </div>
     </div>
