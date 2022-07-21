b12666d919a4a2232b47e11ed59885faac45bd03
April Reitan <45362322+areitan@users.noreply.github.com>
2022-07-20 22:53:25 -0700

    fixing header

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,23 +4,16 @@ import background from '../assets/pexels-rovenimagescom-949587.jpg'
 
 const styles = {
   header: {
-    maxWidth: "300px",
-  },
-  myName: {
-    justifyText: "right",
+    maxWidth: "300px"
   }
 };
 
 function Header() {
   return (
-    <div style={{ backgroundImage: `url(${background})`, backgroundRepeat: 'no-repeat',
-    backgroundSize:'cover' }}>
-      <div style={styles.myName}>
-      April Reitan
-      </div>
+    <div style={{ backgroundImage: `url(${background})`, backgroundRepeat: 'no-repeat', backgroundSize: 'cover' }}>
       <div style={styles.header}>
-      <img style={{ width: 225, height: 300 }} src={myPic} alt="It's me, April!"/>
-    </div>
+        <img style={{ width: 225, height: 300 }} src={myPic} alt="It's me, April!" />
+      </div>
     </div>
   );
 }
